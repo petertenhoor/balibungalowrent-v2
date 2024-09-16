c62c47c6ef4cc3ac2c97a29e64ed4db4f1e6b806
petertenhoor <P_T_H_92@hotmail.com>
2024-09-16 16:07:22 +0200

    Automatically get rid of second single-room-attributes block if one is already present in Gutenberg content

--- public/wp-content/themes/booklium-child/functions.php
+++ public/wp-content/themes/booklium-child/functions.php
@@ -18,21 +18,21 @@ function booklium_child_wp_enqueue_scripts()
     }
 }
 
-//add_filter('the_content', 'booklium_child_the_content', 100, 1);
-//
-///**
-// * Remove room type attributes from the content if 'mphb-single-room-type-attributes' appears twice
-// * @param string $content
-// * @return string
-// */
-//function booklium_child_the_content(string $content): string
-//{
-//    if (is_singular('mphb_room_type')) {
-//        $count = preg_match_all('/mphb-single-room-type-attributes/', $content);
-//        if ($count > 1) {
-//            $content = preg_replace('/<div class="single-room-attributes-wrapper">.*?<\/div>/s', '', $content);
-//        }
-//    }
-//
-//    return $content;
-//}
+add_filter('the_content', 'booklium_child_the_content', 100, 1);
+
+/**
+ * Remove room type attributes from the content if 'mphb-single-room-type-attributes' appears twice
+ * @param string $content
+ * @return string
+ */
+function booklium_child_the_content(string $content): string
+{
+    if (is_singular('mphb_room_type')) {
+        $count = preg_match_all('/mphb-single-room-type-attributes/', $content);
+        if ($count && is_numeric($count) && $count > 1) {
+            $content = preg_replace('/<div class="single-room-attributes-wrapper">.*?<\/div>/s', '', $content);
+        }
+    }
+
+    return $content;
+}
