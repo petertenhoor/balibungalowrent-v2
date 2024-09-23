1a7a976075535a555ca357c731fb14bcd281b189
petertenhoor <P_T_H_92@hotmail.com>
2024-09-23 15:25:21 +0200

    Add custom block pattern bottom feature for single accomodation and location term pages

--- public/wp-content/themes/booklium-child/functions.php
+++ public/wp-content/themes/booklium-child/functions.php
@@ -1,8 +1,11 @@
 <?php
 
-// enqueue parent stylesheet
-add_action('wp_enqueue_scripts', 'booklium_child_wp_enqueue_scripts');
-function booklium_child_wp_enqueue_scripts()
+
+/**
+ * Enqueue the parent and child theme stylesheets
+ * @return void
+ */
+function initBookliumChild(): void
 {
 
     $parent_theme = wp_get_theme(get_template());
@@ -18,14 +21,14 @@ function booklium_child_wp_enqueue_scripts()
     }
 }
 
-add_filter('the_content', 'booklium_child_the_content', 100, 1);
+add_action('wp_enqueue_scripts', 'initBookliumChild');
 
 /**
  * Remove room type attributes from the content if 'mphb-single-room-type-attributes' appears twice
  * @param string $content
  * @return string
  */
-function booklium_child_the_content(string $content): string
+function removeDoubleRoomAttributesBlock(string $content): string
 {
     if (is_singular('mphb_room_type')) {
         $count = preg_match_all('/mphb-single-room-type-attributes/', $content);
@@ -36,3 +39,71 @@ function booklium_child_the_content(string $content): string
 
     return $content;
 }
+
+add_filter('the_content', 'removeDoubleRoomAttributesBlock', 100, 1);
+
+/**
+ * Modify the wp_block post type to be public
+ * @return void
+ */
+function makeWpBlockTypePublicInAdmin(): void
+{
+    global $wp_post_types;
+
+    if (is_admin() && isset($wp_post_types['wp_block'])) {
+        $wp_post_types['wp_block']->public = true;
+    }
+}
+
+add_action('init', 'makeWpBlockTypePublicInAdmin', 99);
+
+//add settings for accommodations
+if (function_exists('acf_add_options_page')) {
+    add_action('acf/init', function () {
+        acf_add_options_sub_page([
+            'page_title' => __("Custom settings", 'booklium'),
+            'menu_title' => __("Custom settings", 'booklium'),
+            'menu_slug' => 'custom-accommodation-settings',
+            'post_id' => 'custom-accommodation-settings',
+            'parent_slug' => sprintf('edit.php?post_type=%1$s', 'mphb_room_type'),
+        ]);
+    });
+}
+
+//add some HTML above the footer when on single mphb_room_type post
+add_action('acf/init', function () {
+    add_action('get_footer', function () {
+        $blockPatternPostContent = '';
+
+        //handle single accommodation
+        if (is_singular('mphb_room_type')) {
+            $accommodationBottomContentGlobalId = get_field('accommodation_bottom_content_global', 'custom-accommodation-settings') ?: false;
+            if ($accommodationBottomContentGlobalId) {
+                $blockPatternPost = get_post($accommodationBottomContentGlobalId);
+                if (is_a($blockPatternPost, 'WP_Post')) {
+                    $blockPatternPostContent = apply_filters('the_content', $blockPatternPost->post_content);
+                }
+            }
+        }
+
+        //handle mphb_ra_locations term pages
+        if (is_tax('mphb_ra_locations')) {
+            $term = get_queried_object();
+            $locationBottomContentId = get_field('location_bottom_content', $term) ?: false;
+            if ($locationBottomContentId) {
+                $blockPatternPost = get_post($locationBottomContentId);
+                if (is_a($blockPatternPost, 'WP_Post')) {
+                    $blockPatternPostContent = apply_filters('the_content', $blockPatternPost->post_content);
+                }
+            }
+        }
+
+        if ($blockPatternPostContent): ?>
+            <section class='content-area content-area--bottom' style="padding-bottom: 0 !important">
+                <div class="entry-content">
+                    <?php echo $blockPatternPostContent; ?>
+                </div>
+            </section>
+        <?php endif;
+    });
+});
